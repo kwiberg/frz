kwiberg/frz

commit d73e568236db1916a89be458aba305c774979f4d

`frz add`: git-add symlinks instead of ignoring them
Fixes https://github.com/kwiberg/frz/issues/3.

src/top_directory.cc
@@ -60,10 +60,14 @@ class TopDirectory final {
           create_hasher_(std::move(create_hasher)),
           hash_name_(std::move(hash_name)) {}
 
-    bool AddFile(const std::filesystem::path& file, int subdir_levels) {
+    Top::AddResult AddFile(const std::filesystem::path& file,
+                           int subdir_levels) {
+        CreateHashdirSymlink(file.parent_path(), subdir_levels);
+        if (std::filesystem::is_symlink(file)) {
+            return Top::AddResult::kSymlink;
+        }
         FRZ_ASSERT(std::filesystem::is_regular_file(
             std::filesystem::symlink_status(file)));
-        CreateHashdirSymlink(file.parent_path(), subdir_levels);
         auto source = CreateFileSource(file);
         SizeHasher hasher(create_hasher_());
         streamer_.Stream(*source, hasher);
@@ -78,7 +82,8 @@ class TopDirectory final {
         if (!inserted) {
             unused_content_store_->MoveInsert(content_path);
         }
-        return inserted;
+        return inserted ? Top::AddResult::kNewFile
+                        : Top::AddResult::kDuplicateFile;
     }
 
     Top::FillResult Fill(Log& log,
@@ -429,7 +434,7 @@ class TopDirectoryCache final : public Top {
           create_hasher_(std::move(create_hasher)),
           hash_name_(std::move(hash_name)) {}
 
-    bool AddFile(const std::filesystem::path& file) override {
+    AddResult AddFile(const std::filesystem::path& file) override {
         const TopDirRef& t = GetTopDir(file);
         return t.topdir->AddFile(file, t.level);
     }
@@ -455,7 +460,7 @@ class TopDirectoryCache final : public Top {
     };
 
     const TopDirRef& GetTopDir(const std::filesystem::path& path) try {
-        std::filesystem::path p = std::filesystem::canonical(path);
+        std::filesystem::path p = std::filesystem::weakly_canonical(path);
         return GetTopDir(
             /*canonical_dir=*/std::filesystem::is_directory(p)
                 ? p

src/command.cc
@@ -112,19 +112,21 @@ int Add(CommonArgs& common_args, const AddArgs& add_args) {
     auto add_file = [&](const std::filesystem::directory_entry& dent) {
         if (std::filesystem::is_directory(dent.symlink_status())) {
             return;
-        } else if (!std::filesystem::is_regular_file(dent.symlink_status())) {
+        } else if (!std::filesystem::is_regular_file(dent.symlink_status()) &&
+                   !dent.is_symlink()) {
             ++nonfiles;
             return;
         }
-        const bool inserted = common_args.top->AddFile(dent.path());
-        if (inserted) {
+        const Top::AddResult r = common_args.top->AddFile(dent.path());
+        if (r == Top::AddResult::kNewFile) {
             ++successful;
-        } else {
+            absl::PrintF("+ %s\n", pretty_path(dent.path()));
+        } else if (r == Top::AddResult::kDuplicateFile) {
             ++duplicates;
+            absl::PrintF("= %s\n", pretty_path(dent.path()));
         }
         git->Add(dent.path());  // don't use plain `dent` here, since AddFile()
                                 // will have replaced the file with a symlink
-        absl::PrintF("%s %s\n", inserted ? "+" : "=", pretty_path(dent.path()));
     };
     for (const auto& file : add_args.files) {
         try {

src/command_add_test.cc
@@ -34,6 +34,7 @@ using ::testing::_;
 using ::testing::AllOf;
 using ::testing::Each;
 using ::testing::ElementsAre;
+using ::testing::Eq;
 using ::testing::Not;
 using ::testing::SizeIs;
 using ::testing::StartsWith;
@@ -139,6 +140,32 @@ TEST_P(TestCommandAdd1, DirectoryTree) {
     EXPECT_THAT(d.Path() / "sub/sume",
                 AllOf(IsSymlinkWhoseTarget(StartsWith(".frz/blake3/")),
                       ReadContents(StrEq("fff")),
+                      UseGit() ? GitStatus(ElementsAre("index_new"))
+                               : Not(GitStatus(_))));
+}
+
+TEST_P(TestCommandAdd1, AddSymlinks) {
+    TempDir d;
+    d.Dir(".frz");
+    if (UseGit()) {
+        CreateGitRepository(d.Path());
+    }
+    d.Symlink("one", "1");
+    d.Symlink("sub/two", "2");
+
+    if (AddWithDot()) {
+        EXPECT_EQ(0, Command(d.Path(), {"add", "."}));
+    } else {
+        EXPECT_EQ(0, Command(d.Path(), {"add", "one", "sub"}));
+    }
+
+    EXPECT_THAT(d.Path() / "one",
+                AllOf(IsSymlinkWhoseTarget(Eq("1")),
+                      UseGit() ? GitStatus(ElementsAre("index_new"))
+                               : Not(GitStatus(_))));
+    EXPECT_THAT(d.Path() / "sub/.frz", IsSymlinkWhoseTarget(StrEq("../.frz")));
+    EXPECT_THAT(d.Path() / "sub/two",
+                AllOf(IsSymlinkWhoseTarget(Eq("2")),
                       UseGit() ? GitStatus(ElementsAre("index_new"))
                                : Not(GitStatus(_))));
 }

src/top_directory.hh
@@ -46,7 +46,12 @@ class Top {
     // with a symlink). Return true if the file contents were used, false if
     // they were ignored because the content directory already had an identical
     // file.
-    virtual bool AddFile(const std::filesystem::path& file) = 0;
+    enum class AddResult {
+        kNewFile,
+        kDuplicateFile,
+        kSymlink,
+    };
+    virtual AddResult AddFile(const std::filesystem::path& file) = 0;
 
     // Identify and attempt to fill missing content in the frz repository that
     // owns `path`. `content_sources` lists directories that we may copy or

commit c4441d26e49dffd12f7df788685de5d598ee427d

Fix bug introduced in d73e568236db

src/filesystem_util.cc
@@ -47,6 +47,11 @@ std::optional<std::filesystem::path> RelativeSubtreePath(
     }
 }
 
+std::filesystem::path NonLeafCanonical(const std::filesystem::path& path) {
+    const std::filesystem::path p = path.lexically_normal();
+    return std::filesystem::canonical(p.parent_path()) / p.filename();
+}
+
 bool IsReadonly(const std::filesystem::file_status& status) {
     return (status.permissions() & kAllWritePermissions) ==
            std::filesystem::perms::none;

src/filesystem_util.hh
@@ -28,6 +28,12 @@ namespace frz {
 std::optional<std::filesystem::path> RelativeSubtreePath(
     const std::filesystem::path path, const std::filesystem::path subtree_root);
 
+// Convert `path` to a canonical absolute path, i.e. an absolute path that has
+// no dot, dot-dot elements or symbolic links in its generic format
+// representation, _except_ that if the leaf (the rightmost path element) is a
+// symlink, leave it as it is and do not dereference it.
+std::filesystem::path NonLeafCanonical(const std::filesystem::path& path);
+
 // Does this file lack all write prmissions?
 bool IsReadonly(const std::filesystem::file_status& status);
 

src/top_directory.cc
@@ -460,7 +460,7 @@ class TopDirectoryCache final : public Top {
     };
 
     const TopDirRef& GetTopDir(const std::filesystem::path& path) try {
-        std::filesystem::path p = std::filesystem::weakly_canonical(path);
+        std::filesystem::path p = NonLeafCanonical(path);
         return GetTopDir(
             /*canonical_dir=*/std::filesystem::is_directory(p)
                 ? p